100efa8bc7bcb1dbc19813cc9173325fbbd0b8e3
Bogdan Solovei <51416784+bsolovei25@users.noreply.github.com>
2020-04-28 16:16:10 +0300

    Update demo_module.js

--- demo_module.js
+++ demo_module.js
@@ -18,6 +18,7 @@ con.connect(function(err) {
   var sql = "SELECT * from TODOList where address = "+mysql.escape(addr);
   con.query(sql, function (err, result,fields) {
     if (err) throw err;
+    
     myvariable = JSON.stringify(result);
     //console.log();
   });
