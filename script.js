062851f352df6453b01cddabf874cf4c2063ddf2
Bohdan Solovei <bsolovei25@gmail.com>
2020-04-28 16:11:56 +0300

    heroku commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var PORT = process.env.PORT || 5000;
 var http = require('http');
 var dt = require('./demo_module.js');
 
@@ -5,7 +6,7 @@ var dt = require('./demo_module.js');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Blue Lagune element' + dt.MyFunc());
-}).listen(8080);
+}).listen(PORT);
 
 /*exports.myDateTime = function () {
     return Date();
