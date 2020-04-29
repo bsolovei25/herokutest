48bd049a924beb01082954959b58b834fa09a4be
Bogdan Solovei <51416784+bsolovei25@users.noreply.github.com>
2020-04-29 14:32:05 +0300

    Update demo_module.js

--- demo_module.js
+++ demo_module.js
@@ -1,14 +1,25 @@
-//var mysql = require('mysql');
-var mysql = require('./node_modules/mysql/index.js');
+var mysql = require('mysql');
+var express = require("express");
+//var mysql = require('./node_modules/mysql/index.js');
 
 var myvariable;
 
-var con = mysql.createConnection({
+/*var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database:"mydb"
+});*/
+
+
+var con = mysql.createConnection({
+  host: "us-cdbr-iron-east-01.cleardb.net",
+  user: "b04aeb8f8cadb3",
+  password: "1a305679",
+  database:"heroku_7ac9d03fb56fb24"
 });
+
+
 
 con.connect(function(err) {
   if (err) throw err;
