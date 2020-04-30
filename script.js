805e6fe8f5667218183f50ce5c43e307711291d6
Bogdan Solovei <51416784+bsolovei25@users.noreply.github.com>
2020-04-30 11:48:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 var PORT = process.env.PORT || 5000;
 var http = require('http');
-//var dt = require('./demo_module.js');
+var express = require('express');
+var app = express();
+var dt = require('./demo_module.js');
 
-http.createServer(function (req, res) {
+app.listen(PORT,()=>{
+  console.log('Server has started')
+})
+
+app.get('/',(req, res)=>{
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Blue Lagune element' + dt.MyFunc());
-}).listen(PORT);
+})
+
+/*http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Blue Lagune element' + dt.MyFunc());
+}).listen(PORT);*/
